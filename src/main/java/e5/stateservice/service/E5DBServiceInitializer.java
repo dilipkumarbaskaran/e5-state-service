b032d2d68ab0e4e544686aa4fd13e97ebac052ce
Dilipkumar Baskaran <dilipkumar.baskaran@e5.ai>
2025-04-21 14:28:02 +0530

    Enabled HikariCP connection pool

--- src/main/java/e5/stateservice/service/E5DBServiceInitializer.java
+++ src/main/java/e5/stateservice/service/E5DBServiceInitializer.java
@@ -69,8 +69,6 @@ public class E5DBServiceInitializer {
             settings.put("jakarta.persistence.schema-generation.database.action", "create-drop");
         }
         settings.put("hibernate.show_sql", "false");
-        settings.put("hibernate.query.plan_cache_max_size", "10");
-        settings.put("hibernate.query.plan_parameter_metadata_max_size", "10");
 
         var serviceRegistry = new StandardServiceRegistryBuilder()
                 .applySettings(settings).build();
